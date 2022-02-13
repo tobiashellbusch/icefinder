b3775ffdc2f192a6b5bf8eee7eee8128c47fdd71
tobiashellbusch <t.hellbusch@hotmail.de>
2022-02-13 13:54:20 +0100

    provide db query script

---
+++ db/makedict.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import requests
+
+def goforward(s, i, text):
+    for k in range(i+1, len(text) - len(s) - 1):
+        if text[k:k+len(s)] == s:
+            return k
+    return -1
+
+# feed in a saved html page of the results from https://www.fernbahn.de/datenbank/suche/
+# searching for type ICE and the current year
+def get_allnumbers():
+    with open("liste22.html", "r") as file:
+        liste = file.read()
+    p = 0
+    nrs = []
+    while p != -1:
+        p = goforward("ICE<", p, liste)
+        if p == -1:break
+        p = goforward("<td", p, liste)
+        if p == -1:break
+        p = goforward(">", p, liste)
+        if p == -1:break
+        p2 = goforward("<", p, liste)
+        if p2 == -1:break
+        nrs.append(int(liste[p+1:p2]))
+    return nrs
+    
+
+alldict = {}
+allnumbers = get_allnumbers() # list(range(1, 1732)) + [1980, 9510] + list(range(9550, 9599)) + list(range(52980, 52991))
+
+for i in range(100000): # avoid doubles found in allnumbers
+    if i not in allnumbers:
+        continue
+    
+    nr = str(i)
+    k = 0
+    i_done = 0
+    while i_done < allnumbers.count(i):
+        if k == 15: # irgendwann reichts
+            print(f"nothing found for {i}")
+            break
+        k += 1
+        zugid = "20220" + str(k) + "00000"[:-len(nr)] + nr; # Jahr+01+Zugnummer(5 stell.)
+        db_url = "https://www.fernbahn.de/datenbank/suche/?zug_id=" + zugid;
+
+        contents = requests.get(db_url).content.decode('utf-8')
+        if "Es konnten keine Datensätze mit Ihren Suchparametern gefunden werden." in contents:
+            continue
+
+        off = goforward("ICE-Typ", 0, contents)
+        typoff = goforward("ICE", off, contents)
+        typend = goforward("<", typoff, contents)
+        if off == -1 or typoff == -1 or typend == -1:
+            continue
+        typ = contents[typoff:typend]
+        if i not in alldict:
+            alldict[i] = typ
+            print(f"{nr}.{k}\t{typ}")
+        else:
+            alldict[i] += ";\n" + typ
+            print(" " * len(nr) + f".{k}\t{typ}")
+        i_done += 1
+
+with open("icedict22.txt", "w") as file:
+    file.write(alldict.__repr__())
